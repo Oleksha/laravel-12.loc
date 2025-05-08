90740de658f83bbda7e5061c37b0c0ae7d90c1e3
Oleksha <deryabino@gmail.com>
2025-05-08 10:13:48 +0400

    8. Laravel 12 middleware with multiple roles

---
+++ app/Http/Middleware/SuperAdminMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Auth;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class SuperAdminMiddleware
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check()) {
+            if (Auth::check()->is_role == 2) {
+                return $next($request);
+            } else {
+                Auth::logout();
+                return redirect(url('login'));
+            }
+        } else {
+            Auth::logout();
+            return redirect(url('login'));
+        }
+    }
+}
---
+++ app/Http/Middleware/UserMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Auth;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserMiddleware
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check()) {
+            if (Auth::check()->is_role == 0) {
+                return $next($request);
+            } else {
+                Auth::logout();
+                return redirect(url('login'));
+            }
+        } else {
+            Auth::logout();
+            return redirect(url('login'));
+        }
+    }
+}
