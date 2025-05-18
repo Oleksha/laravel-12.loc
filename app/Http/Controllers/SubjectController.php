ae33e92b4b82631f8a0aaafa05629af7b24dc0f4
Oleksha <deryabino@gmail.com>
2025-05-18 13:08:39 +0400

    30. Subjects insert a record in database

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Subject;
 use App\Models\Teacher;
+use Illuminate\Http\Request;
 
 class SubjectController extends Controller
 {
@@ -11,5 +12,20 @@ class SubjectController extends Controller
     {
         $data['getRecord'] = Subject::getRecord();
         return view('superadmin.subjects.index', $data);
+    }
+
+    public function add()
+    {
+        return view('superadmin.subjects.add');
+    }
+
+    public function store(Request $request)
+    {
+        $save = new Subject();
+        $save->name = trim($request->name);
+        $save->description = trim($request->description);
+        $save->save();
+        return redirect('superadmin/subjects/list')
+            ->with('success', 'Record successfully create');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -56,8 +56,8 @@ Route::group(['middleware' => 'superadmin'], function () {
     Route::get('superadmin/teachers/delete/{id}', [TeacherController::class, 'destroy']);
     // Subjects
     Route::get('superadmin/subjects/list', [SubjectController::class, 'index']);
-    Route::get('superadmin/subjects/add', [TeacherController::class, 'add']);
-    Route::post('superadmin/subjects/add', [TeacherController::class, 'store']);
+    Route::get('superadmin/subjects/add', [SubjectController::class, 'add']);
+    Route::post('superadmin/subjects/add', [SubjectController::class, 'store']);
     Route::get('superadmin/subjects/edit/{id}', [TeacherController::class, 'edit']);
     Route::post('superadmin/subjects/edit/{id}', [TeacherController::class, 'update']);
     Route::get('superadmin/subjects/delete/{id}', [TeacherController::class, 'destroy']);
